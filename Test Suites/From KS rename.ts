f436af1c43efb7fd59b74e8d667b54add09e1c13
NhiLy <nhi.ly@katalon.com>
2025-04-14 16:09:15 +0700

    rename from KS

--- Test Suites/From KS rename.ts
+++ Test Suites/From KS rename.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>From KS</name>
+   <name>From KS rename</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
