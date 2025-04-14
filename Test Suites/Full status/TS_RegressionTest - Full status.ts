8c87cf9549fcdbea1d3dc72cd503776ba5037cf1
NhiLy <nhi.ly@katalon.com>
2025-04-14 13:49:42 +0700

    commit all

--- Test Suites/Full status/TS_RegressionTest - Full status.ts
+++ Test Suites/Full status/TS_RegressionTest - Full status.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TS_RegressionTest - Full statues</name>
+   <name>TS_RegressionTest - Full status</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
