975ee82536976bd9cc52f926e6b718f150817dcb
NhiLy <nhi.ly@katalon.com>
2025-04-14 16:13:06 +0700

    abc

--- Test Suites/From KS rename abc.ts
+++ Test Suites/From KS rename abc.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>From KS rename</name>
+   <name>From KS rename abc</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
