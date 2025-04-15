471d6dc635c9cc3157b59a45f28bb6ce31a2863a
NhiLy <nhi.ly@katalon.com>
2025-04-15 17:33:02 +0700

    update tsc

--- Test Suites/TestSuiteFromTO/TO-TestSuiteCollection.ts
+++ Test Suites/TestSuiteFromTO/TO-TestSuiteCollection.ts
@@ -15,7 +15,7 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/Data Binding/TO Execution module</testSuiteEntity>
+         <testSuiteEntity>Test Suites/Full status/TO Execution module</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
