acfed0346e1c329906d3818c6f900efa131c749c
NhiLy <nhi.ly@katalon.com>
2025-04-15 11:28:26 +0700

    add new folder

--- Test Suites/TestSuiteFromTO/_(),.-_ are allowed edit name of TOs test suite.ts
+++ Test Suites/TestSuiteFromTO/_(),.-_ are allowed edit name of TOs test suite.ts
@@ -1,7 +1,7 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-   <name>_(),.-_ are allowed edit name of TOs test suite</name>
    <description></description>
+   <name>_(),.-_ are allowed edit name of TOs test suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
